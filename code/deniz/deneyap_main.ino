8d1e15df8476d01859b524194267dc8d53224fb7
pyreneos <62503221+pyreneos@users.noreply.github.com>
2023-08-29 13:23:48 +0300

    Add files via upload

--- code/deniz/deneyap_main.ino
+++ code/deniz/deneyap_main.ino
@@ -17,7 +17,7 @@ typedef struct dataPack {
   int8_t f_b_speed = 0;  //speed -100 100
   int8_t horizontal;     //-1 right 0 forward 1 left
 } dataPack;
-
+void OnDataRecv(const uint8_t* mac, const uint8_t* incomingData, int len);
 dataPack instance;
 
 //tcs3200 renk okuma
@@ -35,21 +35,33 @@ bool fnc_tcs34725_iscolor(int h, int _color);
 
 
 //Motor sürücü chunk
-uint8_t motor_1_pwm = D0;
-uint8_t motor_1_forward = D6;
-uint8_t motor_1_backward = D7;
+uint8_t m_1_1pwm = 16;
+uint8_t m_1_1f = 2;
+uint8_t m_1_1b = 4;
 
-uint8_t motor_2_pwm = D1;
-uint8_t motor_2_forward = D8;
-uint8_t motor_2_backward = D9;
+uint8_t m_1_2pwm = 17;
+uint8_t m_1_2f = 36;
+uint8_t m_1_2b = 34;
+
+uint8_t m_2_1pwm = 23;
+uint8_t m_2_1f = 5;
+uint8_t m_2_1b = 13;
+
+uint8_t m_2_2pwm = 27;
+uint8_t m_2_2f = 12;
+uint8_t m_2_2b = 14;
 
 
-const uint16_t freq = 2000;
+const uint16_t freq = 5000;
 const uint8_t pwmChannel1 = 0;
 const uint8_t pwmChannel2 = 1;
+const uint8_t resolution = 10;
 
-void motor_speed(uint8_t motor_pin, uint8_t speed);          // Problematic due to channel and ledcwrite
-void motor_way(uint8_t motor_pin, uint8_t motor_forw_back);  //forward 1 backward 0
+void motor_speed(uint8_t motor_pin, uint8_t speed);  // Problematic due to channel and ledcwrite
+void m_r_f();
+void m_l_f();
+void m_r_b();
+void m_l_b();
 
 //Deneyap analog and digital pins
 uint8_t i[] = { A0, A1, A2, A3, A4, A5 };
@@ -79,49 +91,50 @@ void setup() {
 
   //Motor init
   ledcSetup(pwmChannel1, freq, resolution);
-  ledcAttachPin(motor_1_pwm, pwmChannel);
+  ledcAttachPin(m_1_1pwm, pwmChannel1);
+  ledcAttachPin(m_2_1pwm, pwmChannel1);
   ledcSetup(pwmChannel2, freq, resolution);
-  ledcAttachPin(motor_2_pwm, pwmChannel);
-  pinMode(D6, OUTPUT);
-  pinMode(D7, OUTPUT);
-  pinMode(D8, OUTPUT);
-  pinMode(D9, OUTPUT);
-  //Motor way init
-  motor_way(D0);
-  motor_way(D1);
+  ledcAttachPin(m_1_2pwm, pwmChannel2);
+  ledcAttachPin(m_2_2pwm, pwmChannel2);
 
+  //way pins
+  pinMode(m_1_1f, OUTPUT);
+  pinMode(m_1_1b, OUTPUT);
+  pinMode(m_1_2f, OUTPUT);
+  pinMode(m_1_2b, OUTPUT);
+  pinMode(m_2_1f, OUTPUT);
+  pinMode(m_2_1b, OUTPUT);
+  pinMode(m_2_2f, OUTPUT);
+  pinMode(m_2_2b, OUTPUT);
+  //Motor way init
+  m_r_f();
+  m_l_f();
   //Robot kol init
-  servo_D2.attach(, 1, 50);
+  /*servo_D2.attach(, 1, 50);
   servo_D3.attach(, 1, 50);
   servo_D4.attach(, 1, 50);
-  pinMode(D5, INPUT);
+  */
 }
 
 
 void loop() {
   if (instance.an_dig_state == 0) {
-    temp_speed = map(instance.f_b_speed, , 100, ) if (instance.horizontal == 0) {
-      if (!((digitalRead(motor_1_forward) == HIGH) && (digitalRead(motor_2_forward) == HIGH))) {
-        motor_way(D0);
-        motor_way(D1);
-      }
+    if (instance.horizontal == 0) {
+      m_r_f();
+      m_l_f();
 
-      motor_speed(0, instance.f_b_speed);
-      motor_speed(1, instance.f_b_speed);
-    }
-    else if (instance.horizontal == 1) {
-      if (!((digitalRead(motor_1_forward) == HIGH) && (digitalRead(motor_2_forward) == LOW))) {
-        motor_way(D0, 1);
-        motor_way(D1, 0);
-      }
+      motor_speed(0, abs(instance.f_b_speed));
+      motor_speed(1, abs(instance.f_b_speed));
+    } else if (instance.horizontal == 1) {
+      m_r_f();
+      m_l_b();
+
       motor_speed(0, 100);
       motor_speed(1, 100);
-    }
-    else if (instance.horizontal == -1) {
-      if (!((digitalRead(motor_1_forward) == HIGH) && (digitalRead(motor_2_forward) == LOW))) {
-        motor_way(D0, 0);
-        motor_way(D1, 1);
-      }
+    } else if (instance.horizontal == -1) {
+      m_r_b();
+      m_l_f();
+
       motor_speed(0, 100);
       motor_speed(1, 100);
     }
@@ -129,37 +142,24 @@ void loop() {
     //motor hızlarını rescale ediyoruz
     F_B_axis = map(instance.X_axis, 0, 4095, -60, 60);  //düz hız
     R_L_axis = map(instance.X_axis, 0, 4095, -60, 60);  // sağ sola göre motor hızını değiştiren parametre
-    if (R_L_axis < 0) {
-      L_motor = F_B_axis + R_L_axis; //Sağ motor
-      R_motor = F_B_axis - R_L_axis; // Sol motor
-      if (L_motor >= 0) {
-        motor_way(D0);
-      } else {
-        motor_way(D0, 0);
-      }
-      if (R_motor >= 0) {
-        motor_way(D1);
-      } else {
-        motor_way(D1, 0);
-      }
+
+    L_motor = F_B_axis + R_L_axis;  //Sağ motor
+    R_motor = F_B_axis - R_L_axis;  // Sol motor
+    if (L_motor >= 0) {
+      m_l_f();
     } else {
-      L_motor = F_B_axis + R_L_axis;
-      R_motor = F_B_axis - R_L_axis;
-      if (L_motor >= 0) {
-        motor_way(D0);
-      } else {
-        motor_way(D0, 0);
-      }
-      if (R_motor >= 0) {
-        motor_way(D1);
-      } else {
-        motor_way(D1, 0);
-      }
+      m_l_b();
     }
-    motor_speed(0,abs(R_speed));
-    motor_speed(1,abs(L_speed));
+    if (R_motor >= 0) {
+      m_r_f();
+    } else {
+      m_r_b();
+    }
+    motor_speed(0, abs(R_motor));
+    motor_speed(1, abs(L_motor));
   }
 }
+
 
 //Belki kullanılabilir.
 void robot_arm_controller(int16_t angle_1, int16_t angle_2, bool grab_drop) {
@@ -177,27 +177,59 @@ void robot_arm_controller(int16_t angle_1, int16_t angle_2, bool grab_drop) {
 void motor_speed(uint8_t channel, uint8_t speed) {
   ledcWrite(channel, speed);
 }
-
-void motor_way(uint8_t motor_pin, uint8_t motor_forw_back = 1) {
+void m_r_f() {
+  digitalWrite(m_1_1f, HIGH);
+  digitalWrite(m_2_1f, HIGH);
+  digitalWrite(m_1_1b, LOW);
+  digitalWrite(m_2_1b, LOW);
+}
+void m_l_f() {
+  digitalWrite(m_1_2f, HIGH);
+  digitalWrite(m_2_2f, HIGH);
+  digitalWrite(m_1_2b, LOW);
+  digitalWrite(m_2_2b, LOW);
+}
+void m_r_b() {
+  digitalWrite(m_1_1f, LOW);
+  digitalWrite(m_2_1f, LOW);
+  digitalWrite(m_1_1b, HIGH);
+  digitalWrite(m_2_1b, HIGH);
+}
+void m_l_b() {
+  digitalWrite(m_1_2f, LOW);
+  digitalWrite(m_2_2f, LOW);
+  digitalWrite(m_1_2b, HIGH);
+  digitalWrite(m_2_2b, HIGH);
+}
+/*
+void motor_way(uint8_t motor_pin, uint8_t motor_forw_back) {
   if (motor_pin == D0) {
     if (motor_forw_back == 1) {
+      digitalWrite(motor_1_forward, HIGH);
+      digitalWrite(motor_1_backward, LOW);
       digitalWrite(motor_1_forward, HIGH);
       digitalWrite(motor_1_backward, LOW);
     } else if (motor_forw_back == 0) {
       digitalWrite(motor_1_forward, LOW);
       digitalWrite(motor_1_backward, HIGH);
+      digitalWrite(motor_1_forward, LOW);
+      digitalWrite(motor_1_backward, HIGH);
     }
-  } else if (motor_pin == D1) {
+  } else {
     if (motor_forw_back == 1) {
-      digitalWrite(motor_2_forward, HIGH);
-      digitalWrite(motor_2_backward, LOW);
+      digitalWrite(motor_1_forward, LOW);
+      digitalWrite(motor_1_backward, HIGH);
+      digitalWrite(motor_1_forward, LOW);
+      digitalWrite(motor_1_backward, HIGH);
     } else if (motor_forw_back == 0) {
+      digitalWrite(motor_2_forward, LOW);
+      digitalWrite(motor_2_backward, HIGH);
       digitalWrite(motor_2_forward, LOW);
       digitalWrite(motor_2_backward, HIGH);
     }
   }
 }
-
+*/
 void OnDataRecv(const uint8_t* mac, const uint8_t* incomingData, int len) {
   memcpy(&instance, incomingData, sizeof(instance));
 }
